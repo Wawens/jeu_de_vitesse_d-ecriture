ac897e440fa07e22512083dec34bc096fc45bc2f
Wawens Janvier <janvierwawens@gmail.com>
2022-03-23 06:25:38 +0000

    Un jeu qui est fait a base d'api qui va venir calculer ton niveau de vitesse decriture

---
+++ js/index.js
@@ -0,0 +1,93 @@
+const apicall = "http://api.quotable.io/random";
+
+const tempsAffche = document.querySelector('.temps');
+const scoreAffche = document.querySelector('.score');
+const phraseEcrire = document.querySelector('.phraseAEcrire');
+const phraseTest = document.querySelector('.phrase-test');
+const p = document.querySelector('.p');
+//
+let temps = 60;
+let score = 0;
+let phrasePourScore;
+tempsAffche.innerText = 'Temps :' + temps;
+scoreAffche.innerText = 'Score :' + score;
+
+
+phraseEcrire.addEventListener('copy', (copyText))
+
+function copyText() {
+    let copy = document.getElementById("demo")
+    copy.innerHTML = "Tu as copié, je pensais pourvoir compter sur ta sincérité ..!";
+    setTimeout(() => {
+        copy.style.display = "none";
+    }, 3000);
+
+}
+
+
+
+// Mettre un start
+let timer = setInterval(time, 1000);
+
+function time() {
+    temps--
+    tempsAffche.innerText = 'Votre Temps :' + temps;
+    scoreAffche.innerText = 'Score :' + score;
+    if (temps === 0) {
+        clearInterval(timer)
+        p.innerText = 'Vous avez Pu totaliser un Score de: ' + score + " points" + " En 60s ";
+
+    }
+}
+
+// Prendre une phrase de l'api
+
+async function afficherNvPhrase() {
+    const appel = await fetch(apicall);
+    const resultats = await appel.json();
+    console.log(resultats.content)
+    const phrase = resultats.content;
+    phrasePourScore = phrase.length;
+    //
+    phraseEcrire.innerHTML = "";
+    phrase.split("").forEach(carac => {
+        const caracSpan = document.createElement("span");
+        caracSpan.innerText = carac;
+        phraseEcrire.appendChild(caracSpan);
+    });
+    phraseTest.value = null;
+}
+afficherNvPhrase()
+
+phraseTest.addEventListener("input", () => {
+    const tableauPhrase = phraseEcrire.querySelectorAll('span');
+    const tableauTest = phraseTest.value.split('');
+
+    let correct = true;
+
+    tableauPhrase.forEach((caracSpan, index) => {
+        console.log(caracSpan)
+        const caractere = tableauTest[index];
+
+        if (caractere === undefined) {
+            caracSpan.classList.remove('correct');
+            caracSpan.classList.remove('incorrect');
+            correct = false;
+        } else if (caractere === caracSpan.innerText) {
+            caracSpan.classList.add('correct');
+            caracSpan.classList.remove('incorrect');
+        } else {
+            caracSpan.classList.remove('correct');
+            caracSpan.classList.add('incorrect');
+            correct = false;
+        }
+
+    })
+    if (correct) {
+        afficherNvPhrase();
+        score += phrasePourScore;
+    } else {
+        score -= phrasePourScore;
+    }
+
+})
